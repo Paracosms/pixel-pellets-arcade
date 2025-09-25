5d380ca5d66ae134bdb7fc12a8d9601ab5db700c
Paracosms <ahiponia@protonmail.com>
2025-09-25 00:08:03 -0700

    added raindrop attack

--- scenes/bosses/boss3/atk1.gd
+++ scenes/bosses/boss3/atk1.gd
@@ -6,10 +6,10 @@ extends Node
 @export var BULLETSPEED : int = 150
 
 @onready var bulletScene = preload("res://scenes/bullet.tscn")
+@onready var raindropAttackScene = preload("res://scenes/bosses/boss3/raindropBullet.tscn")
 @onready var boss = get_parent()
 @onready var stopPosition : Vector2 = Vector2(boss.global_position.x, 50)
 
-var timeAlive : float = 0
 var isStopped : bool = false
 var rotating : bool = true
 var direction : int
@@ -29,6 +29,10 @@ func getMovementPattern():
 	boss.isLookingAtPlayer = false
 
 func getVelocity() -> Vector2:
+	if boss.global_position.y != stopPosition.y:
+		return (stopPosition - boss.global_position).normalized() * SPEED * 5
+	
+	
 	# direction is always final pos - initial pos
 	if direction == -1 :
 		# player is on the left; move left
@@ -37,55 +41,14 @@ func getVelocity() -> Vector2:
 		# player is on the right; move right
 		return Vector2(1,0) * SPEED
 
-func spawnBullet(angle):
-	var bullet = bulletScene.instantiate()
-	
-	# cos to get x vector, sin to get y vector
-	var angleVector = Vector2(cos(angle), sin(angle))
-	
-	# sets individual bullet properties
-	bullet.timesBounced = Globals.bulletBouncesBeforeDeath - 2
-	bullet.position = boss.global_position # position of the spawner
-	bullet.rotation = angle
-	bullet.velocity = angleVector * BULLETSPEED
-	bullet.baseVelocity = bullet.velocity
-	bullet.name = "bullet"
-	
-	boss.get_parent().add_child(bullet)
-	Globals.currentBullets.push_front(bullet)
-
-
-
-func spawnWave():
-	# shoot at 30, 150, 270 degrees, but accounted for radians and imperfect integer equilateral triangle angles
-	var angles = [0.525851, 2.619592, 4.700081]
-	# for each angle in angles[], spawn an individualized bullet using that angle value
-	for angle in angles:
-			spawnBullet(angle + boss.rotation)
-
 # spawn 3 waves of bullets, rotate, spawn 3 waves of bullets
 func attack():
-	for i in 3:
-		%waveTimer.start()
-		await %waveTimer.timeout # spawns wave
-	
-	await get_tree().create_timer(0.5).timeout # cooldown before it rotates again
-	%rotateTransitionTimer.start()
-	finalRotation = boss.rotation + PI/3
+	var raindrop = raindropAttackScene.instantiate()
+	raindrop.position = boss.position
+	boss.get_parent().add_child(raindrop)
+	Globals.debug(raindrop)
 
-func updateBulletVelocities():
-	var periodicFunction = -asin(cos(cos(timeAlive))) + 2.2
-	var velocityMultiplier = Vector2(periodicFunction, periodicFunction)
-	
-	Globals.currentBullets = Globals.currentBullets.filter(func(n): return is_instance_valid(n) and !(n is Node and n.is_queued_for_deletion()))
-	
-	for bulletNode in Globals.currentBullets:
-		bulletNode.velocity = bulletNode.baseVelocity * velocityMultiplier
-
-func _physics_process(delta: float) -> void:
-	
-	timeAlive += delta
-	updateBulletVelocities()
+func _physics_process(_delta: float) -> void:
 	
 	# if the boss hits a wall, turn around
 	if boss.position.x > 1920 || boss.position.x < 0:
@@ -93,11 +56,12 @@ func _physics_process(delta: float) -> void:
 	
 	# start spawning bullets once end position reached
 	if !isStopped && (stopPosition - boss.global_position).length() <= 3:
+		%attackTimer.start()
 		boss.global_position = stopPosition
 		isStopped = true
 		
-	if (stopPosition - boss.global_position).length() <= 3:
-		attack()
+	#if (stopPosition - boss.global_position).length() <= 3:
+	#	%attackTimer.start()
 		
 	
 	if (rotating && boss.rotation < finalRotation):
---
+++ scenes/bosses/boss3/raindropBullet.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+@export var timeBetweenBulletInitialization : float = 0.05
+@export var flashesBeforeFalling : int = 2
+@export var timeBetweenFlashes : float = 0.3
+@export var BULLETSPEED : int = 400
+
+@onready var bulletScene = preload("res://scenes/bullet.tscn")
+
+@onready var telegraphSpawnpoints : Array = %telegraphSpawnpoints.get_children()
+var bullets : Array
+
+func _ready() -> void:
+	# initialize real bullets (disabled)
+	for spawnpoint in telegraphSpawnpoints:
+		var bullet = bulletScene.instantiate()
+		bullet.position = spawnpoint.position
+		bullet.name = "bullet"
+		bullet.visible = false
+		bullets.push_front(bullet)
+		bullet.tree_exited.connect(func(): bullets.erase(bullet))
+		add_child(bullet)
+	
+	# place each spawnpoint right after the other
+	for spawnpoint in telegraphSpawnpoints:
+		await get_tree().create_timer(timeBetweenBulletInitialization).timeout
+		spawnpoint.visible = true
+	
+	# place real bullets behind placeholders
+	for bullet in bullets:
+		bullet.visible = true
+	
+	# flash a few times before falling
+	for i in flashesBeforeFalling + 1:
+		await get_tree().create_timer(timeBetweenFlashes).timeout
+		
+		for spawnpoint in telegraphSpawnpoints:
+			spawnpoint.visible = true
+		
+		await get_tree().create_timer(timeBetweenFlashes).timeout
+		
+		for spawnpoint in telegraphSpawnpoints:
+			spawnpoint.visible = false
+	
+	for spawnpoint in telegraphSpawnpoints:
+		spawnpoint.visible = false
+	
+	# make the raindrop fall
+	for bullet in bullets:
+		bullet.velocity = Vector2(0, 1) * BULLETSPEED
+
+func _physics_process(_delta: float) -> void:
+	if bullets.is_empty():
+		queue_free()
